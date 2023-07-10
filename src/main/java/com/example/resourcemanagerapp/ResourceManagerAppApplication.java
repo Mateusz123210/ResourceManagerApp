52ba391c711a3e7a2bd503ad9aedae940daebd9d
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2023-07-10 10:22:06 +0200

    Created empty SpringBoot project

---
+++ src/main/java/com/example/resourcemanagerapp/ResourceManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.resourcemanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resourcemanagerapp/ResourceManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourcemanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
